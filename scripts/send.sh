7b61b7dd8c07a753d1a7bc3ad0a1c9384e5e6361
hankei6km <gh-hankei6km@outlook.jp>
2022-03-16 17:07:29 +0900

    Use "$SA"

--- scripts/send.sh
+++ scripts/send.sh
@@ -9,9 +9,9 @@ FILE_ID="$("${GDRIVE}" --service-account "${SA}" -c . \
     sed "1d" | grep "${DEST_FILE_NAME}" | head -n 1 | cut -d" " -f1)"
 
 if test -z "${FILE_ID}" ; then
-  "${GDRIVE}" --service-account gha-creds-test-temp.json  -c . \
+  "${GDRIVE}" --service-account "${SA}"  -c . \
     upload --no-progress --parent "${PARENT_ID}" --name "${DEST_FILE_NAME}" "${SRC_FILE_NAME}"
 else
-  "${GDRIVE}" --service-account gha-creds-test-temp.json  -c . \
+  "${GDRIVE}" --service-account "${SA}"  -c . \
     update --no-progress "${FILE_ID}" --name "${DEST_FILE_NAME}" "${SRC_FILE_NAME}"
 fi
